2c902333d2618f8cdc7787dffb095720a3b2f533
Comrade-Ivan <1335308@gmail.com>
2021-03-26 11:39:47 +0300

    Задание с таблицей

---
+++ tasks/tables.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Таблица</title>
+</head>
+<body>
+  <table border="1">
+    <thead>
+      <tr>
+        <th align="center" rowspan="2">Номер</th>
+        <th align="center" colspan="2">Тэги</th>
+      </tr>
+      <tr>
+        <th align="center">тег</th>
+        <th align="center">значение</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td align="center">1</td>
+        <td align="center">p</td>
+        <td rowspan="4" align="center">для текста</td>
+      </tr>
+      <tr>
+        <td align="center">2</td>
+        <td align="center">a</td>
+      </tr>
+      <tr>
+        <td align="center">3</td>
+        <td align="center">b</td>
+      </tr>
+      <tr>
+        <td align="center">4</td>
+        <td align="center">i</td>
+      </tr>
+      <tr>
+        <td align="center">5</td>
+        <td align="center">table</td>
+        <td align="center">таблица</td>
+      </tr>
+      <tr>
+        <td align="center">6</td>
+        <td align="center">tr</td>
+        <td align="center">ряд таблицы</td>
+      </tr>
+      <tr>
+        <td align="center">7</td>
+        <td align="center">td</td>
+        <td align="center" rowspan="2">ячейки таблицы</td>
+      </tr>
+      <tr>
+        <td align="center">8</td>
+        <td align="center">th</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
