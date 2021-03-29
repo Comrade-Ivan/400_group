42d9b834cf7ec2222bdd0462737ac996e61fb303
Comrade-Ivan <1335308@gmail.com>
2021-03-29 10:28:55 +0300

    CSS 1

---
+++ css/css-basics.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Основы CSS</title>
+  <link rel="stylesheet" href="./style/css-basics.css">
+  <style>
+    #paragraph-two { /* # - находим элемент по id */
+      color: blue;
+      font-size: 25px;
+    }
+  </style>
+</head>
+<body>
+  <p style="color: red;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, dicta.</p>
+  <p id="paragraph-two">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, dicta.</p>
+  <p class="paragraph-three">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, dicta.</p>
+  <h1>Заголовок h1</h1>
+</body>
+</html>
