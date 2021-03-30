3d7796b554180fe0b52694dcffa7c05c5c9f2c8d
Comrade-Ivan <1335308@gmail.com>
2021-03-30 09:30:52 +0300

    svetofor

---
+++ tasks/lab3/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Светофор</title>
+  <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+  <div class="box">
+    <div class="red square"></div>
+    <div class="yellow square"></div>
+    <div class="green square"></div>
+  </div>
+</body>
+</html>
