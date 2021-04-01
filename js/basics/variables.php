58a3b84fc4602041e7c0c38d6703936c0e4ebe73
Comrade-Ivan <1335308@gmail.com>
2021-04-01 13:05:40 +0300

    Variables

---
+++ js/basics/variables.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Переменные и типы данных</title>
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <script> /*Перед закрывающимся тэгом body*/
+    //console.log("Привет, мир!"); /*Вывод в консоль*/
+    //alert("Привет, мир!"); /*Вывод в виде всплывающего окна*/
+    //Однострочный комментарий
+    /*
+      Многострочный 
+      Комментарий
+    */
+  </script>
+  <script src="./variables.js"></script>
+</body>
+</html>
