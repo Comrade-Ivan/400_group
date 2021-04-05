5345372821e184bc5397a3b2525a703c2da8fb83
Comrade-Ivan <1335308@gmail.com>
2021-04-05 18:02:21 +0300

    Условия и циклы

---
+++ js/basics/if-for.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Условия и циклы</title>
+</head>
+<body>
+  <script src="if-for.js"></script>
+</body>
+</html>
---
+++ tasks/lab6/script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+//Task 1
+// let a = 10;
+// let b = 3;
+// let result = 10 % 3;
+// console.log(result);
+// //Task 2
+// let a2 = 9;
+// let b2 = 6;
+// let result2 = a2 + b2;
+// console.log(result2);
+// //Task 3
+// const dollar = 76.40;
+// const euro = 89.82;
+// let rubles = 1000;
+// let convertedDollar = rubles/dollar;
+// let convertedEuro = rubles/euro;
+// alert(convertedDollar + "\n" + convertedEuro);
+// //Task 4
+// let a3 = 10;
+// let b3 = 15;
+// let h = 10;
+// let square = ((a3 + b3) / 2) * h;
+// console.log(`Площадь трапеции равна ${square} см`);
+
+//Отладка
+
+let price = 57;
+let count = 4;
+let days = 31;
+let sum = price * count;
+if(typeof sum === "number"){
+  sum *= days;
+}
+alert("стоимость за " + days + " день = " + sum);
+
+
+
+
