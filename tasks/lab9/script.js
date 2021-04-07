b2d2d0294046dc77dad30059c869f1dbdf884f62
Comrade-Ivan <1335308@gmail.com>
2021-04-07 09:28:07 +0300

    lab9

---
+++ tasks/lab9/script.js
@@ -0,0 +1,21 @@
+//task 1
+function sqrt(number) {
+  return number ** 2;
+}
+console.log( sqrt(3) );
+
+//task 2
+function isEqual(a, b) {
+  return a == b;
+}
+
+//task 3
+function calc(a, b, c) {
+  return (a - b) / c;
+}
+
+//task 4
+
+function sayGoodbye(userName) {
+  alert(`До свидания, ${userName}`);
+}
